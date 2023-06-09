18fa8f68b62efa61f2758346da9fe06b2ae7641f
srinivas-b-sindagi <apple@Apples-MacBook-Pro.local>
2023-06-09 16:15:43 +0530

    add missing folder prefix

--- udocker/config.py
+++ udocker/config.py
@@ -18,7 +18,7 @@ class Config(object):
     """
     conf = {}
     conf['verbose_level'] = 3
-    conf['homedir'] = "/tmp"
+    conf['homedir'] = "/tmp/.udocker"
     conf['topdir'] = conf['homedir']
     conf['bindir'] = None
     conf['libdir'] = None
